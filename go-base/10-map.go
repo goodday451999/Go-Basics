2e62a918cbcceae151f9acc08c84ec1db9e93fd6
Nilotpal Pramanik <33095476+goodday451999@users.noreply.github.com>
2020-12-12 10:19:56 +0530

    Update 10-map.go

--- go-base/10-map.go
+++ go-base/10-map.go
@@ -1,1 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main(){
+	m := make(map[string]int)
+	
+	m["abc"] = 1
+	m["def"] = 2
+	m["ghi"] = 3
+	
+	fmt.Println(m)
+	fmt.Println(m["def"])
+	
+	delete(m, "def")
+	fmt.Println(m)
+	  
+}
+
 
