8df1212b460090b5d880c84436ab88d7ae6e0554
Nilotpal Pramanik <33095476+goodday451999@users.noreply.github.com>
2020-12-12 07:55:20 +0530

    Create 2-variables.go

---
+++ go-base/2-variables.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main(){
+	var x_int int = 5
+	fmt.Println(x_int)
+	
+	x_float := 3.9
+	fmt.Println(x_float)
+	
+	x_string := "it's Great"
+	fmt.Println(x_string)
+}
