a73c66b686941bcc72dcbeaa1afcbc8438fbea07
Nilotpal Pramanik <33095476+goodday451999@users.noreply.github.com>
2021-01-04 16:33:39 +0530

    Update 5-functions.go

--- go-base/5-functions.go
+++ go-base/5-functions.go
@@ -4,6 +4,11 @@ import (
 	"fmt"
 )
 
+func op(x, y int) (int, int) {
+	var output1 = x + y
+	var output2 = y - x
+	return output1, output2
+}
 func doSum(x int, y int) int {
 	return x + y
 }
@@ -12,6 +17,10 @@ func main(){
 	x := 2
 	y := 3
 	sum := doSum(x, y)
-	fmt.Println("Sum: ", sum) 	 
+	fmt.Println("Sum: ", sum) 
+	
+	add, sub := op(x, y)
+	fmt.Println("Add: ", add)
+	fmt.Println("Sub: ", sub)
 }
 
