a78d2856c3500e40c1d9f1e1effa90f42dc4a842
Nilotpal Pramanik <33095476+goodday451999@users.noreply.github.com>
2021-01-17 14:36:35 +0530

    Update 14-file.go

--- go-base/14-file.go
+++ go-base/14-file.go
@@ -35,3 +35,24 @@ func closeFile(f *os.File) {
   }
 }
   
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+func main (){
+  f := createFile("") // no path is defined
+  defer closeFile(f)
+  wrteFile(f)
+}
+
+func createFile(path string) *os.File {
+  fmt.Println("Creating")         // creating - will be printing - same as -
+  defer fmt.Println("Created")    // created
+  f, err := os.Create(path)
+  if err != nil {
+    panic(err)                    // then the panic err will be printed
+  }
+  return f
+}
+
+
+// once panic is encountered the whole prog. will stop
