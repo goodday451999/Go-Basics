16cca97fdf31bddb29fd05a3b2f73fc66221f0d6
Nilotpal Pramanik <33095476+goodday451999@users.noreply.github.com>
2021-01-17 14:46:46 +0530

    Create 15-panic.go

---
+++ go-base/15-panic.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+  "fmt"
+)
+
+func accountDetails(userId *int, ifsc *int) {
+  fmt.Println("%d  %d\n", *userId, *ifsc)
+  fmt.Println("from func")
+}
+
+func main() {
+  userId := 1112
+  ifsc := 24
+  
+  accountDetails(&userId, &ifsc)
+  fmt.Prinlnln("from main")
+  
+}
+
+////////////////////////////
+
+1112  24
+from func
+from main
+
+
+///////////////////////////
+
+func accountDetails(userId *int, ifsc *int) {
+  defer fmt.Println("defer from func")
+  if userId != nil{
+    panic("userId can't be nil")
+  }
+  if ifsc != nil{
+    panic("ifsc can't be nil")
+  }
+  fmt.Println("%d  %d\n", *userId, *ifsc)
+  fmt.Println("from func")
+}
+
+func main() {
+  defer fmt.Println("defer from main")
+  userId := 1112
+  //ifsc := 24
+  
+  accountDetails(&userId, null)
+  fmt.Prinlnln("from main")
+  
+}
+
+/////////////////////////////
+
+defer from func
+defer from main
+ifsc can't be nil
+
+////////////////////////////
+
+once panic occured execution stop but check for defer to print then panic will be printed at end
